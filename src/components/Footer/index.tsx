6a45c3900c3c073c2b6d00ca465205d31aa1f658
yuhuakukude <15577334054@163.com>
2022-11-24 02:28:54 +0800

    Improve

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,21 +7,34 @@ import node from '../../assets/images/node_icon.png'
 import earn from '../../assets/images/earn_icon.png'
 import sport from '../../assets/images/sport_icon.png'
 import { useI18n } from 'react-simple-i18n'
+import { ExternalLink } from '../../theme/components'
 
 interface TabContent {
   title: string
   icon: string
   router: string
+  link?: string
 }
 
-function TabButton({ icon, title, router }: { icon: string; title: string; router: string }) {
+function TabButton({ icon, title, router, link }: { icon: string; title: string; router: string; link?: string }) {
   return (
-    <Link to={router}>
-      <Stack spacing={6} alignItems={'center'}>
-        <Image width={'26vw'} src={icon} />
-        <Typography>{title}</Typography>
-      </Stack>
-    </Link>
+    <>
+      {link ? (
+        <ExternalLink href={link}>
+          <Stack spacing={6} alignItems={'center'}>
+            <Image width={'26vw'} src={icon} />
+            <Typography>{title}</Typography>
+          </Stack>
+        </ExternalLink>
+      ) : (
+        <Link to={router}>
+          <Stack spacing={6} alignItems={'center'}>
+            <Image width={'26vw'} src={icon} />
+            <Typography>{title}</Typography>
+          </Stack>
+        </Link>
+      )}
+    </>
   )
 }
 
@@ -32,7 +45,7 @@ export default function Footer() {
     { title: t('home.title'), icon: home, router: './home' },
     { title: t('node.title'), icon: node, router: './node' },
     { title: t('earn.title'), icon: earn, router: './earn' },
-    { title: t('sport.title'), icon: sport, router: './sport' }
+    { title: t('sport.title'), icon: sport, router: './sport', link: 'https://dapp.formulanets.info' }
   ]
   return (
     <ShowOnMobile>
@@ -45,8 +58,8 @@ export default function Footer() {
         justifyContent={'space-around'}
         padding={'10px 0px'}
       >
-        {Tabs.map(({ title, icon, router }) => {
-          return <TabButton key={title} icon={icon} title={title} router={router} />
+        {Tabs.map(({ title, icon, router, link }) => {
+          return <TabButton key={title} icon={icon} title={title} router={router} link={link} />
         })}
       </Stack>
     </ShowOnMobile>
